8b8c726eaf7e1e3873c3e240b5da757a33fdf2cb
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2019-12-07 19:12:37 +0600

    Full-Featured Web App Part 2 - Templates

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [{
+    'author': 'Tahmid Tanzim Lupin',
+    'date_posted': '25th March, 2019',
+    'title': 'Lorem Ipsum',
+    'content': 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'
+},{
+    'author': 'Tahmid Tanzim Lupin',
+    'date_posted': '25th March, 2019',
+    'title': 'Lorem Ipsum',
+    'content': 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'
+}]
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, World!</h1>'
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
