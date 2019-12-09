ce41cd805ee881b2c2b9d6513c681fd6b650a6bf
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2019-12-09 18:38:47 +0600

    Full-Featured Web App Part 3 - Forms and User Input

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 * pipenv shell
 * pip3 install Flask
 * python app.py
+* pip3 install flask-wtf
+
+imports secrets
+secrets.token_hex(16)
--- app.py
+++ app.py
@@ -1,27 +1,50 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e3f6db2fe4ec99f04db9b1c1ab0cc55f'
 
 posts = [{
     'author': 'Tahmid Tanzim Lupin',
     'date_posted': '25th March, 2019',
     'title': 'Lorem Ipsum',
     'content': 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'
-},{
+}, {
     'author': 'Tahmid Tanzim Lupin',
     'date_posted': '25th March, 2019',
     'title': 'Lorem Ipsum',
     'content': 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'
 }]
 
+
 @app.route('/')
 @app.route('/home')
-def home():
+def dashboard():
     return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def signup():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('dashboard'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def signin():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login success', 'success')
+        return redirect(url_for('dashboard'))
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
